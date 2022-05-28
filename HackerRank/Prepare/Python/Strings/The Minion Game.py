5c855cf1fc656296a312815d22e4a653bef4158c
Taehyun Kim <k1m743hyun@icloud.com>
2022-05-28 06:53:00 +0000

    Create The Minion Game.py

---
+++ HackerRank/Prepare/Python/Strings/The Minion Game.py
@@ -0,0 +1,22 @@
+def minion_game(string):
+    # your code goes here
+    vowels = 'AEIOU'
+    
+    result = {'Stuart': 0, 'Kevin': 0}
+    for start in range(len(string)):
+        if string[start] in vowels:
+            result['Kevin'] += len(string) - start
+        else:
+            result['Stuart'] += len(string) - start
+    
+    if result['Stuart'] > result['Kevin']:
+        print('Stuart', str(result['Stuart']))
+    elif result['Stuart'] < result['Kevin']:
+        print('Kevin', str(result['Kevin']))
+    else:
+        print('Draw')
+    
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
