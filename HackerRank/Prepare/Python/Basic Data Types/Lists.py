8c39b296d7c36ede21080319910ee92ccf10f438
Taehyun Kim <k1m743hyun@icloud.com>
2022-05-28 05:46:11 +0000

    Create Lists.py

---
+++ HackerRank/Prepare/Python/Basic Data Types/Lists.py
@@ -0,0 +1,30 @@
+if __name__ == '__main__':
+    N = int(raw_input())
+    
+    result = []
+    for _ in range(N):
+        line = raw_input().split()
+        
+        if line[0] == 'insert':
+            result.insert(int(line[1]), int(line[2]))
+            
+        elif line[0] == 'print':
+            print(result)
+            
+        elif line[0] == 'remove':
+            result.remove(int(line[1]))
+        
+        elif line[0] == 'append':
+            result.append(int(line[1]))
+            
+        elif line[0] == 'sort':
+            result.sort()
+            
+        elif line[0] == 'pop':
+            result.pop()
+            
+        elif line[0] == 'reverse':
+            result.reverse()
+        
+        else:
+            pass
