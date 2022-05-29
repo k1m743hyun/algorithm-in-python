cd2e1747a462418cad3d15e6749da6ecfc1e10c3
Taehyun Kim <k1m743hyun@icloud.com>
2022-05-29 05:44:34 +0000

    Create Counting Valleys.py

---
+++ HackerRank/Prepare/Interview Preparation Kit/Warm-up Challenges/Counting Valleys.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    level = 0
+    result = []
+    for p in path:
+        if p == 'U':
+            level += 1
+        else:
+            level -= 1
+        
+        result.append(level)
+    
+    cnt = 0
+    for i in range(len(result)-1):
+        if result[i] < 0 and result [i+1] == 0:
+            cnt += 1
+            
+    return cnt
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
