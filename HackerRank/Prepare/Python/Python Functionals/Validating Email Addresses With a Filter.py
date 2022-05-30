c94376283d564032258881447e4ab548efd76d13
Taehyun Kim <k1m743hyun@icloud.com>
2022-05-30 06:37:45 +0000

    Create Validating Email Addresses With a Filter.py

---
+++ HackerRank/Prepare/Python/Python Functionals/Validating Email Addresses With a Filter.py
@@ -0,0 +1,18 @@
+import re
+
+def fun(s):
+    # return True if s is a valid email, else return False
+    return True if re.compile('^[A-Za-z0-9\-\_]+\@[A-Za-z0-9]+\.[A-Za-z]{1,3}$').fullmatch(s) else False
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)
