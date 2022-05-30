45cda89771d5473647c8149662eaf0047856702d
Taehyun Kim <k1m743hyun@icloud.com>
2022-05-30 05:09:29 +0000

    Create Regex Substitution.py

---
+++ HackerRank/Prepare/Python/Regex and Parsing/Regex Substitution.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+n =  int(input())
+
+for _ in range(n):
+    line = input()
+    
+    line = re.sub('(?<= )(\&\&)(?= )', 'and', line)
+    line = re.sub('(?<= )(\|\|)(?= )', 'or', line)
+    
+    print(line)
+    
