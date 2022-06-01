b195b9f3096796c2bd473312c553dd961b13d287
Taehyun Kim <k1m743hyun@icloud.com>
2022-06-01 10:53:12 +0000

    Create sWAP cASE.py

---
+++ HackerRank/Prepare/Python/Strings/sWAP cASE.py
@@ -0,0 +1,7 @@
+def swap_case(s):
+    return ''.join([i if i.isdigit() else i.lower() if i.isupper() else i.upper() for i in s])
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
