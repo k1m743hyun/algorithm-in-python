f541be889b0f600e40a0e99cc42e4f0e4a7915aa
Taehyun Kim <k1m743hyun@icloud.com>
2022-06-02 01:54:41 +0000

    Create String Formatting.py

---
+++ HackerRank/Prepare/Python/Strings/String Formatting.py
@@ -0,0 +1,18 @@
+def print_formatted(number):
+    # your code goes here
+    num_list = [[str(i+1), str(oct(i+1)).replace('0o', ''),
+                    str(hex(i+1)).replace('0x', ''),
+                    str(bin(i+1)).replace('0b', '')]
+                    for i in range(number)]
+    d_list, o_list, h_list, b_list = zip(*num_list)
+    
+    s = len(b_list[-1])
+    for i in range(number):
+        print(d_list[i].rjust(s, ' '),
+                o_list[i].rjust(s, ' '),
+                h_list[i].rjust(s, ' ').upper(),
+                b_list[i].rjust(s, ' '), sep=' ')
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
