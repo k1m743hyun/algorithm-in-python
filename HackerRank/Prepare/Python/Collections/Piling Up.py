eb1625c288a0a2b9f15dd17f338d28315831dd24
Taehyun Kim <k1m743hyun@icloud.com>
2022-06-05 13:02:53 +0000

    Create Piling Up.py

---
+++ HackerRank/Prepare/Python/Collections/Piling Up.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    blocks = list(map(int, input().split()))
+    
+    i = 0
+    while i < len(blocks) - 1 and blocks[i] >= blocks[i+1]:
+        i += 1
+    while i < len(blocks) - 1 and blocks[i] <= blocks[i+1]:
+        i += 1
+    
+    print('Yes' if i == len(blocks) -1 else 'No')
