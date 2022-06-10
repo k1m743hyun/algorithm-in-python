01c39d09150923a2d871a5fefdab1a9a0d9bec64
Taehyun Kim <k1m743hyun@icloud.com>
2022-06-10 07:37:41 +0000

    Create Collections.deque.py

---
+++ HackerRank/Prepare/Python/Collections/Collections.deque.py
@@ -0,0 +1,21 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import deque
+
+n = int(input())
+
+dq = deque()
+for _ in range(n):
+    line = input().split()
+    
+    if 'append' in line[0]:
+        if 'left' in line[0]:
+            dq.appendleft(line[-1])
+        else:
+            dq.append(line[-1])
+    else:
+        if 'left' in line[0]:
+            dq.popleft()
+        else:
+            dq.pop()
+
+print(' '.join(dq))
