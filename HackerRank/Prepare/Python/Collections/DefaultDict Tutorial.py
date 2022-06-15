4355e32ab89c8d822e67c9ef69c15d5a52acd155
Taehyun Kim <k1m743hyun@icloud.com>
2022-06-15 13:29:36 +0000

    Create DefaultDict Tutorial.py

---
+++ HackerRank/Prepare/Python/Collections/DefaultDict Tutorial.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n, m = map(int, input().split())
+
+a = [input() for _ in range(n)]
+b = [input() for _ in range(m)]
+
+for c in b:
+    if c in a:
+        print(' '.join([str(i+1) for i, e in enumerate(a) if e == c]))
+    else:
+        print(-1)
