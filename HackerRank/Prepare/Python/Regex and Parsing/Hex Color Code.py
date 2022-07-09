dd7745599557938ebef1f36808f2646cca1b2f83
Taehyun Kim <k1m743hyun@icloud.com>
2022-07-09 02:01:24 +0000

    Create Hex Color Code.py

---
+++ HackerRank/Prepare/Python/Regex and Parsing/Hex Color Code.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+n = int(input())
+
+color_pattern = '#[A-Fa-f0-9]{3,6}'
+
+code_comp = re.compile('[ :]+'+color_pattern)
+color_comp = re.compile(color_pattern)
+
+result = []
+for _ in range(n):
+    line = input()
+    result += code_comp.findall(line)
+
+for i in map(color_comp.findall, result):
+    print(i[0])
