37b4c0ddcd20e2ea70ea429a52c804c1131e0a0f
Taehyun Kim <k1m743hyun@icloud.com>
2022-07-14 04:54:38 +0000

    Create 크레인 인형뽑기 게임.py

---
+++ programmers/2019 카카오 개발자 겨울 인턴십/크레인 인형뽑기 게임.py
@@ -0,0 +1,17 @@
+def solution(board, moves):
+    answer = 0 
+    stack = []
+    for move in moves:
+        for idx in range(len(board)):
+            if board[idx][move-1]:
+                stack.append(board[idx][move-1])
+                board[idx][move-1] = 0
+            
+                if len(stack) > 1:
+                    if stack[-1] == stack[-2]:
+                        stack.pop()
+                        stack.pop()
+                        answer += 2
+                break
+                
+    return answer
