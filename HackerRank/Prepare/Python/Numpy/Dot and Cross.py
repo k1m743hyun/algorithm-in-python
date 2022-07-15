682b81f8ba47674ba04e73339d0e14a5fcd4078c
Taehyun Kim <k1m743hyun@icloud.com>
2022-07-15 07:48:55 +0000

    Create Dot and Cross.py

---
+++ HackerRank/Prepare/Python/Numpy/Dot and Cross.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+n = int(input())
+
+a = np.array([list(map(int, input().split())) for _ in range(n)])
+b = np.array([list(map(int, input().split())) for _ in range(n)])
+
+print(np.dot(a, b))
