bd4b98200584777ff8a077b22b10ceb97609dcf7
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-07 04:24:55 +0000

    Create HTML Parser - Part 1.py

---
+++ HackerRank/Prepare/Python/Regex and Parsing/HTML Parser - Part 1.py
@@ -0,0 +1,25 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start : " + tag)
+        for ele in attrs:
+            print ('->',ele[0],'>',ele[1])
+    
+    def handle_endtag(self, tag):
+        print("End   : " + tag)
+    
+    def handle_startendtag(self, tag, attrs):
+        print("Empty : " + tag)
+        for ele in attrs:
+            print ('->',ele[0],'>',ele[1])
+  
+html = ""       
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
