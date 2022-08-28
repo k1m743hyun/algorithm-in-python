4d63cee4f1a0e4b850d5031e5956a2b1ce666a3e
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-28 19:32:10 +0900

    Update Finding the percentage.py

--- HackerRank/Prepare/Python/Basic Data Types/Finding the percentage.py
+++ HackerRank/Prepare/Python/Basic Data Types/Finding the percentage.py
@@ -1,11 +1,11 @@
 if __name__ == '__main__':
-    n = int(raw_input())
+    n = int(input())
     student_marks = {}
     for _ in range(n):
-        line = raw_input().split()
+        line = input().split()
         name, scores = line[0], line[1:]
         scores = map(float, scores)
         student_marks[name] = scores
-    query_name = raw_input()
+    query_name = input()
     
     print('{:.2f}'.format(sum(student_marks[query_name]) / len(student_marks[query_name])))
