3d4c0c07a6ce65a1d932fde46571595a783c8015
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-28 19:49:36 +0900

    Update Standardize Mobile Number Using Decorators.py

--- HackerRank/Prepare/Python/Closures and Decorators/Standardize Mobile Number Using Decorators.py
+++ HackerRank/Prepare/Python/Closures and Decorators/Standardize Mobile Number Using Decorators.py
@@ -2,12 +2,15 @@ def wrapper(f):
     def fun(l):
         # complete the function
         f(['+91 {} {}'.format(i[-10:-5], i[-5:]) for i in l])
+
     return fun
+
 
 @wrapper
 def sort_phone(l):
     print(*sorted(l), sep='\n')
 
+
 if __name__ == '__main__':
     l = [input() for _ in range(int(input()))]
-    sort_phone(l) 
+    sort_phone(l)
