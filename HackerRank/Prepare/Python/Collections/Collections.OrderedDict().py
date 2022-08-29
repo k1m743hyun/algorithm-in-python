4f35df981e1a13f43f73fa4e256e00814b1a8911
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-29 10:37:14 +0900

    Update Collections.OrderedDict().py

--- HackerRank/Prepare/Python/Collections/Collections.OrderedDict().py
+++ HackerRank/Prepare/Python/Collections/Collections.OrderedDict().py
@@ -7,11 +7,11 @@ d = OrderedDict()
 for _ in range(n):
     line = input().split()
     k, v = ' '.join(line[:-1]), int(line[-1])
-    
+
     if d.get(k):
         d[k] += int(v)
     else:
         d[k] = v
-        
+
 for k, v in d.items():
     print(k, v)
