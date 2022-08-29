82472b7b763ecf5fbf1bafe18f4ffd8b28f586a7
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-29 10:41:18 +0900

    Update Time Delta.py

--- HackerRank/Prepare/Python/Date and Time/Time Delta.py
+++ HackerRank/Prepare/Python/Date and Time/Time Delta.py
@@ -7,9 +7,12 @@ import re
 import sys
 from datetime import datetime
 
+
 # Complete the time_delta function below.
 def time_delta(t1, t2):
-    return str(int(abs(datetime.strptime(t1, '%a %d %b %Y %H:%M:%S %z') - datetime.strptime(t2, '%a %d %b %Y %H:%M:%S %z')).total_seconds()))
+    return str(int(abs(datetime.strptime(t1, '%a %d %b %Y %H:%M:%S %z') - datetime.strptime(t2,
+                                                                                            '%a %d %b %Y %H:%M:%S %z')).total_seconds()))
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
