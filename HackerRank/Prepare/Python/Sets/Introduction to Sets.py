62804917740944e777729a73360435eecefb55d9
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-29 10:57:24 +0900

    Update Introduction to Sets.py

--- HackerRank/Prepare/Python/Sets/Introduction to Sets.py
+++ HackerRank/Prepare/Python/Sets/Introduction to Sets.py
@@ -1,12 +1,14 @@
 from __future__ import division
 
+
 def average(array):
     # your code goes here
     s = set(array)
     return sum(s) / len(s)
 
+
 if __name__ == '__main__':
-    n = int(raw_input())
-    arr = map(int, raw_input().split())
+    n = int(input())
+    arr = map(int, input().split())
     result = average(arr)
-    print result
+    print(result)
