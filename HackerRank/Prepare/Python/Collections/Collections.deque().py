f2148bbfe01a7ad20002eb7817e6692080be805d
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-29 10:34:59 +0900

    Update Collections.deque().py

--- HackerRank/Prepare/Python/Collections/Collections.deque().py
+++ HackerRank/Prepare/Python/Collections/Collections.deque().py
@@ -6,7 +6,7 @@ n = int(input())
 dq = deque()
 for _ in range(n):
     line = input().split()
-    
+
     if 'append' in line[0]:
         if 'left' in line[0]:
             dq.appendleft(line[-1])
