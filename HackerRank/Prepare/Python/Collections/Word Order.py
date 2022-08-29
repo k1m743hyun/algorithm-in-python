755d0bf0a1bd471962f5486ea8284169af3a90fa
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-29 10:40:37 +0900

    Update Word Order.py

--- HackerRank/Prepare/Python/Collections/Word Order.py
+++ HackerRank/Prepare/Python/Collections/Word Order.py
@@ -3,7 +3,7 @@
 word_dict = {}
 for word in [input() for _ in range(int(input()))]:
     if word not in word_dict:
-        word_dict[word] =  1
+        word_dict[word] = 1
     else:
         word_dict[word] += 1
 
