ecb3d5ba86dc74084219ff6c8467ebc3afbf9ed2
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-29 11:11:20 +0900

    Update 신고 결과 받기.py

--- programmers/2022 KAKAO BLIND RECRUITMENT/신고 결과 받기.py
+++ programmers/2022 KAKAO BLIND RECRUITMENT/신고 결과 받기.py
@@ -4,12 +4,12 @@ def solution(id_list, report, k):
         if report_dict.get(data.split(' ')[-1]):
             report_dict[data.split(' ')[-1]].append(data.split(' ')[0])
         else:
-            report_dict[data.split(' ')[-1]] = [data.split(' ')[0]]    
-    
+            report_dict[data.split(' ')[-1]] = [data.split(' ')[0]]
+
     answer = [0] * len(id_list)
     for idx, val in report_dict.items():
         if len(set(val)) >= k:
             for id in set(val):
                 answer[id_list.index(id)] += 1
-    
+
     return answer
