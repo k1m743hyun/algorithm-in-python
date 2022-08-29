538d40b3f90f443df5ccfe792d81416496e1a0aa
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-29 10:53:46 +0900

    Update HTML Parser - Part 2.py

--- HackerRank/Prepare/Python/Regex and Parsing/HTML Parser - Part 2.py
+++ HackerRank/Prepare/Python/Regex and Parsing/HTML Parser - Part 2.py
@@ -1,25 +1,27 @@
 from html.parser import HTMLParser
 
+
 class MyHTMLParser(HTMLParser):
     def handle_comment(self, data):
         if len(data.split('\n')) != 1:
             print('>>> Multi-line Comment')
-            
+
         else:
             print('>>> Single-line Comment')
-            
-        print(data.replace("\r", "\n"))       
-    
+
+        print(data.replace("\r", "\n"))
+
     def handle_data(self, data):
         if data.strip():
             print('>>> Data')
             print(data)
-  
-html = ""       
+
+
+html = ""
 for i in range(int(input())):
     html += input().rstrip()
     html += '\n'
-    
+
 parser = MyHTMLParser()
 parser.feed(html)
 parser.close()
