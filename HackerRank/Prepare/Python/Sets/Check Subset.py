e511e9188c311706f8fbafd9078e0f8bea3855c1
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-29 10:56:55 +0900

    Update Check Subset.py

--- HackerRank/Prepare/Python/Sets/Check Subset.py
+++ HackerRank/Prepare/Python/Sets/Check Subset.py
@@ -4,8 +4,8 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     a = set(map(int, input().split()))
-    
+
     m = int(input())
     b = set(map(int, input().split()))
-    
+
     print(a.issubset(b))
