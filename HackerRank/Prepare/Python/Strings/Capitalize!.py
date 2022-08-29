af3ece92d04582635ebaf4139a1084b2774381f1
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-29 11:02:26 +0900

    Update Capitalize!.py

--- HackerRank/Prepare/Python/Strings/Capitalize!.py
+++ HackerRank/Prepare/Python/Strings/Capitalize!.py
@@ -6,17 +6,19 @@ import random
 import re
 import sys
 
+
 # Complete the solve function below.
 def solve(s):
     for i in s.split():
         s = s.replace(i, i.capitalize())
-    
+
     return s
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    s = raw_input()
+    s = input()
 
     result = solve(s)
 
