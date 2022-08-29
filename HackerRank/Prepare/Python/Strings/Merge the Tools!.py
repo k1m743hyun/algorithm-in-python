f373cd85d45fdd783497cabf17947846845912ec
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-29 11:03:45 +0900

    Update Merge the Tools!.py

--- HackerRank/Prepare/Python/Strings/Merge the Tools!.py
+++ HackerRank/Prepare/Python/Strings/Merge the Tools!.py
@@ -2,10 +2,11 @@ def merge_the_tools(string, k):
     # your code goes here
     for i in range(0, len(string), k):
         li = []
-        for s in list(string[i:i+k]):
+        for s in list(string[i:i + k]):
             if s not in li:
                 li.append(s)
         print(''.join(li))
+
 
 if __name__ == '__main__':
     string, k = input(), int(input())
