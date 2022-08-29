1c73e9408201666c1c8b7da25a219e3dd162d26f
Taehyun Kim <k1m743hyun@icloud.com>
2022-08-29 10:55:02 +0900

    Update Regex Substitution.py

--- HackerRank/Prepare/Python/Regex and Parsing/Regex Substitution.py
+++ HackerRank/Prepare/Python/Regex and Parsing/Regex Substitution.py
@@ -1,13 +1,12 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import re
 
-n =  int(input())
+n = int(input())
 
 for _ in range(n):
     line = input()
-    
+
     line = re.sub('(?<= )(\&\&)(?= )', 'and', line)
     line = re.sub('(?<= )(\|\|)(?= )', 'or', line)
-    
+
     print(line)
-    
