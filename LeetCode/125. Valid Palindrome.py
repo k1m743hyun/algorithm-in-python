b11f3e3bf6f8561e12e48b734e9cecc28951fedc
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-01 13:19:10 +0900

    Update LeetCode/125. Valid Palindrome.py

--- LeetCode/125. Valid Palindrome.py
+++ LeetCode/125. Valid Palindrome.py
@@ -4,14 +4,4 @@ import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         refined = ''.join(re.compile(r'[A-Za-z0-9]+').findall(s)).lower()
-
-        flag = True
-        for idx, val in enumerate(refined):
-            if idx > len(refined) // 2:
-                break
-
-            if val != refined[len(refined) - idx - 1]:
-                flag = False
-                break
-
-        return flag
+        return refined == refined[::-1]
