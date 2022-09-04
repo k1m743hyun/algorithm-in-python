a1c757477ee1e5df289983aed89a8b4843b2e2ad
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-04 21:22:31 +0900

    Update LeetCode/819. Most Common Word.py

--- LeetCode/819. Most Common Word.py
+++ LeetCode/819. Most Common Word.py
@@ -1,9 +1,10 @@
 import re
+from typing import List
 from collections import Counter
 
 
 class Solution:
-    def mostCommonWord(self, paragraph: str, banned: list[str]) -> str:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         return [word for word in list(map(lambda x: x[0], sorted(
             Counter(map(lambda x: x.lower(), re.compile(r'[A-Za-z]+').findall(paragraph))).items(), key=lambda x: x[1],
             reverse=True))) if word not in banned][0]
