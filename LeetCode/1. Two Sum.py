1237b06fcd266dbfb0d3ca82fd037aa9c1295e8c
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-07 10:56:20 +0900

    Update LeetCode/1. Two Sum.py

--- LeetCode/1. Two Sum.py
+++ LeetCode/1. Two Sum.py
@@ -3,6 +3,8 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, n in enumerate(nums):
-            if target - n in nums[i + 1:]:
-                return [i, i + 1 + nums[i + 1:].index(target - n)]
+        nums_dict = {val: idx for idx, val in enumerate(nums)}
+
+        for idx, num in enumerate(nums):
+            if target - num in nums_dict and idx != nums_dict[target - num]:
+                return [idx, nums_dict[target - num]]
