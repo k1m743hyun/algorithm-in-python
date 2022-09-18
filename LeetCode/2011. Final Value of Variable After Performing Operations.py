cbad4871f61423c68b3e3326d9479b709182a5e3
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-18 09:29:09 +0000

    Create LeetCode/2011. Final Value of Variable After Performing Operations.py

---
+++ LeetCode/2011. Final Value of Variable After Performing Operations.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        result = 0
+        for o in operations:
+            c = list(o)
+            if c[0] == 'X':
+                if c[1] == '+':
+                    result += 1
+                else:
+                    result -= 1
+            else:
+                if c[0] == '+':
+                    result += 1
+                else:
+                    result -= 1
+            
+        return result
+      
