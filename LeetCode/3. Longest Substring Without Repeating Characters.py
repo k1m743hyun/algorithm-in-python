76903280c071c5a6dbd5a874f9fb60c87d838812
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-15 18:04:46 +0900

    Update LeetCode/3. Longest Substring Without Repeating Characters.py

--- LeetCode/3. Longest Substring Without Repeating Characters.py
+++ LeetCode/3. Longest Substring Without Repeating Characters.py
@@ -13,3 +13,17 @@ class Solution:
                 max_len = len(li)
 
         return max_len
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    s = "abcabcbb"
+    print(Solution().lengthOfLongestSubstring(s))  # 3
+
+    # Test Case 2
+    s = "bbbbb"
+    print(Solution().lengthOfLongestSubstring(s))  # 1
+
+    # Test Case 3
+    s = "pwwkew"
+    print(Solution().lengthOfLongestSubstring(s))  # 3
