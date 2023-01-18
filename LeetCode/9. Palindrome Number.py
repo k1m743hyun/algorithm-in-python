0b266bc212a4c189a03b29380e433d900341d02c
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-18 21:33:28 +0900

    Update LeetCode/9. Palindrome Number.py

--- LeetCode/9. Palindrome Number.py
+++ LeetCode/9. Palindrome Number.py
@@ -1,3 +1,17 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x) == str(x)[::-1]
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    x = 121
+    print(Solution().isPalindrome(x))  # true
+
+    # Test Case 2
+    x = -121
+    print(Solution().isPalindrome(x))  # false
+
+    # Test Case 3
+    x = 10
+    print(Solution().isPalindrome(x))  # false
