7146537930690215faefb573fc141411041bccd3
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-20 19:36:24 +0900

    Update 242. Valid Anagram.py

--- LeetCode/242. Valid Anagram.py
+++ LeetCode/242. Valid Anagram.py
@@ -7,3 +7,15 @@ class Solution:
             return True
         else:
             return False
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    s = "anagram"
+    t = "nagaram"
+    print(Solution().isAnagram(s, t))  # true
+
+    # Test Case 2
+    s = "rat"
+    t = "car"
+    print(Solution().isAnagram(s, t))  # true
