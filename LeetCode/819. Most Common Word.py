55409c7500114f5fbab22687789c6acfe12dd259
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-20 19:51:03 +0900

    Update LeetCode/819. Most Common Word.py

--- LeetCode/819. Most Common Word.py
+++ LeetCode/819. Most Common Word.py
@@ -6,3 +6,15 @@ from collections import Counter
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         return Counter([word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]).most_common(1)[0][0]
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+    banned = ["hit"]
+    print(Solution().mostCommonWord(paragraph, banned))  # ball
+
+    # Test Case 2
+    paragraph = "a."
+    banned = []
+    print(Solution().mostCommonWord(paragraph, banned))  # a
