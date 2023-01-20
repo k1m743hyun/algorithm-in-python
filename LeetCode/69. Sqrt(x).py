6c8ca63e5969545471e2a049f967af6867f53927
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-20 14:45:28 +0900

    Update 69. Sqrt(x).py

--- LeetCode/69. Sqrt(x).py
+++ LeetCode/69. Sqrt(x).py
@@ -1,3 +1,13 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         return int(x ** 0.5)
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    x = 4
+    print(Solution().mySqrt(x))  # 2
+
+    # Test Case 1
+    x = 8
+    print(Solution().mySqrt(x))  # 2
