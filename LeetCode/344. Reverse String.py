bb53fae4c8d624a91cc44615350eb0fe347401a0
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-20 19:41:54 +0900

    Update 344. Reverse String.py

--- LeetCode/344. Reverse String.py
+++ LeetCode/344. Reverse String.py
@@ -7,3 +7,15 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         s.reverse()
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    s = ["h", "e", "l", "l", "o"]
+    Solution().reverseString(s)
+    print(s)  # ["o", "l", "l", "e", "h"]
+
+    # Test Case 2
+    s = ["H", "a", "n", "n", "a", "h"]
+    Solution().reverseString(s)
+    print(s)  # ["h","a","n","n","a","H"]
