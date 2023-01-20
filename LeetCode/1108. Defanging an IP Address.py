850e081c13ca032c3e7d642e501ccb49888b144a
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-20 20:29:16 +0900

    Update LeetCode/1108. Defanging an IP Address.py

--- LeetCode/1108. Defanging an IP Address.py
+++ LeetCode/1108. Defanging an IP Address.py
@@ -1,4 +1,13 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
         return address.replace('.', '[.]')
-      
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    address = "1.1.1.1"
+    print(Solution().defangIPaddr(address))  # 1[.]1[.]1[.]1
+
+    # Test Case 2
+    address = "255.100.50.0"
+    print(Solution().defangIPaddr(address))  # 255[.]100[.]50[.]0
