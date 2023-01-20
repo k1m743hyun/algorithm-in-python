b8004378813b5ab019e98b29a197995078416e2c
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-20 19:43:47 +0900

    Update 771. Jewels and Stones.py

--- LeetCode/771. Jewels and Stones.py
+++ LeetCode/771. Jewels and Stones.py
@@ -1,3 +1,15 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         return sum([1 for stone in stones for jewel in jewels if jewel == stone])
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    jewels = "aA"
+    stones = "aAAbbbb"
+    print(Solution().numJewelsInStones(jewels, stones))  # 3
+
+    # Test Case 2
+    jewels = "z"
+    stones = "ZZ"
+    print(Solution().numJewelsInStones(jewels, stones))  # 0
