118c28c79438ad684e0e834faa806746f191ce88
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-20 19:26:21 +0900

    Update 125. Valid Palindrome.py

--- LeetCode/125. Valid Palindrome.py
+++ LeetCode/125. Valid Palindrome.py
@@ -5,3 +5,16 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         refined = ''.join(re.compile(r'[A-Za-z0-9]+').findall(s)).lower()
         return refined == refined[::-1]
+
+if __name__ == '__main__':
+    # Test Case 1
+    s = "A man, a plan, a canal: Panama"
+    print(Solution().isPalindrome(s)) # true
+
+    # Test Case 2
+    s = "race a car"
+    print(Solution().isPalindrome(s)) # false
+
+    # Test Case 3
+    s = " "
+    print(Solution().isPalindrome(s)) # true
