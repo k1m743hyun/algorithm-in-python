9817673d7969f45c4b9c7431677f85253a574b0e
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-20 20:25:19 +0900

    Update LeetCode/937. Reorder Data in Log Files.py

--- LeetCode/937. Reorder Data in Log Files.py
+++ LeetCode/937. Reorder Data in Log Files.py
@@ -3,7 +3,6 @@ from typing import List
 
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-
         letter_logs, digit_logs = [], []
         for log in logs:
             if log.split()[1].isdigit():
@@ -12,3 +11,13 @@ class Solution:
                 letter_logs.append(log)
 
         return sorted(letter_logs, key=lambda x: (x.split()[1:], x.split()[0])) + digit_logs
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
+    print(Solution().reorderLogFiles(logs))  # ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+
+    # Test Case 2
+    logs = ["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"]
+    print(Solution().reorderLogFiles(logs))  # ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
