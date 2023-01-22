18903bbc9e66e36920c6cbe869bd7f47b14f8c65
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-22 20:04:04 +0900

    Update LeetCode/1313. Decompress Run-Length Encoded List.py

--- LeetCode/1313. Decompress Run-Length Encoded List.py
+++ LeetCode/1313. Decompress Run-Length Encoded List.py
@@ -10,3 +10,13 @@ class Solution:
             else:
                 result += [val] * freq
         return result
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    nums = [1, 2, 3, 4]
+    print(Solution().decompressRLElist(nums))  # [2, 4, 4, 4]
+
+    # Test Case 2
+    nums = [1, 1, 2, 3]
+    print(Solution().decompressRLElist(nums))  # [1, 3, 3]
