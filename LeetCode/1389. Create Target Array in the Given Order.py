8c577ecd496800360bd7841d9a285d816c4b92be
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-24 19:48:45 +0900

    Update LeetCode/1389. Create Target Array in the Given Order.py

--- LeetCode/1389. Create Target Array in the Given Order.py
+++ LeetCode/1389. Create Target Array in the Given Order.py
@@ -7,3 +7,15 @@ class Solution:
         for i in range(len(nums)):
             result.insert(index[i], nums[i])
         return result
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    nums = [0, 1, 2, 3, 4]
+    index = [0, 1, 2, 2, 1]
+    print(Solution().createTargetArray(nums, index))  # [0, 4, 1, 3, 2]
+
+    # Test Case 2
+    nums = [1, 2, 3, 4, 0]
+    index = [0, 1, 2, 3, 0]
+    print(Solution().createTargetArray(nums, index))  # [0, 1, 2, 3, 4]
