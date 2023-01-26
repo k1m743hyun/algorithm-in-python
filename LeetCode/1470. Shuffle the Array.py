1a61eed23ba1917dbe677faf56b6d0f086baf0b9
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-26 23:27:08 +0900

    Update LeetCode/1470. Shuffle the Array.py

--- LeetCode/1470. Shuffle the Array.py
+++ LeetCode/1470. Shuffle the Array.py
@@ -12,3 +12,20 @@ class Solution:
             result.append(y[i])
 
         return result
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    nums = [2, 5, 1, 3, 4, 7]
+    n = 3
+    print(Solution().shuffle(nums, n))  # [2, 3, 5, 4, 1, 7]
+
+    # Test Case 2
+    nums = [1, 2, 3, 4, 4, 3, 2, 1]
+    n = 4
+    print(Solution().shuffle(nums, n))  # [1,4,2,3,3,2,4,1]
+
+    # Test Case 3
+    nums = [1, 1, 2, 2]
+    n = 2
+    print(Solution().shuffle(nums, n))  # [1,2,1,2]
