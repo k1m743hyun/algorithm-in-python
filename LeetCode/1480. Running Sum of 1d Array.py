50b25730e44bd00b45adec784797d39191b0e74b
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-27 22:00:00 +0900

    Update LeetCode/1480. Running Sum of 1d Array.py

--- LeetCode/1480. Running Sum of 1d Array.py
+++ LeetCode/1480. Running Sum of 1d Array.py
@@ -8,6 +8,19 @@ class Solution:
         for n in nums:
             s += n
             result.append(s)
-        
+
         return result
-      
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    nums = [1, 2, 3, 4]
+    print(Solution().runningSum(nums))  # [1, 3, 6, 10]
+
+    # Test Case 2
+    nums = [1, 1, 1, 1, 1]
+    print(Solution().runningSum(nums))  # [1,2,3,4,5]
+
+    # Test Case 3
+    nums = [3, 1, 2, 10, 1]
+    print(Solution().runningSum(nums))  # [3,4,6,16,17]
