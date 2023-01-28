cf14c101ce2a1f8c29b831cdbc5cd281fbdcaef2
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-28 17:38:51 +0900

    Update LeetCode/1512. Number of Good Pairs.py

--- LeetCode/1512. Number of Good Pairs.py
+++ LeetCode/1512. Number of Good Pairs.py
@@ -4,3 +4,17 @@ from typing import List
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         return sum([1 for j in range(len(nums)) for i in range(len(nums)) if i < j and nums[i] == nums[j]])
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    nums = [1, 2, 3, 1, 1, 3]
+    print(Solution().numIdenticalPairs(nums))  # 4
+
+    # Test Case 2
+    nums = [1, 1, 1, 1]
+    print(Solution().numIdenticalPairs(nums))  # 6
+
+    # Test Case 3
+    nums = [1, 2, 3]
+    print(Solution().numIdenticalPairs(nums))  # 0
