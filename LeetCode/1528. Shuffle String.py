8efdc2d4420fe986b1288f7e98b178981e830121
Taehyun Kim <k1m743hyun@icloud.com>
2023-01-29 16:31:33 +0900

    Update LeetCode/1528. Shuffle String.py

--- LeetCode/1528. Shuffle String.py
+++ LeetCode/1528. Shuffle String.py
@@ -4,3 +4,15 @@ from typing import List
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         return ''.join([v for k, v in sorted({indices[i]: s[i] for i in range(len(s))}.items())])
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    s = "codeleet"
+    indices = [4, 5, 6, 7, 0, 2, 1, 3]
+    print(Solution().restoreString(s, indices))  # leetcode
+
+    # Test Case 2
+    s = "abc"
+    indices = [0, 1, 2]
+    print(Solution().restoreString(s, indices))  # abc
