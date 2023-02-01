78d4fae36a3e782b2e64ebf2ebcede6aab340bd0
Taehyun Kim <k1m743hyun@icloud.com>
2023-02-01 22:12:14 +0900

    Update LeetCode/1672. Richest Customer Wealth.py

--- LeetCode/1672. Richest Customer Wealth.py
+++ LeetCode/1672. Richest Customer Wealth.py
@@ -4,3 +4,13 @@ from typing import List
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         return max([sum(account) for account in accounts])
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    accounts = [[1, 2, 3], [3, 2, 1]]
+    print(Solution().maximumWealth(accounts))  # 6
+
+    # Test Case 2
+    accounts = [[1, 5], [7, 3], [3, 5]]
+    print(Solution().maximumWealth(accounts))  # 10
