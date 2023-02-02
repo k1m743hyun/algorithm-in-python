0da1064176e9a047397df384b061007e7e24afd5
Taehyun Kim <k1m743hyun@icloud.com>
2023-02-02 23:39:15 +0900

    Update LeetCode/1678. Goal Parser Interpretation.py

--- LeetCode/1678. Goal Parser Interpretation.py
+++ LeetCode/1678. Goal Parser Interpretation.py
@@ -1,3 +1,17 @@
 class Solution:
     def interpret(self, command: str) -> str:
         return command.replace('(al)', 'al').replace('()', 'o')
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    command = "G()(al)"
+    print(Solution().interpret(command))  # Goal
+
+    # Test Case 2
+    command = "G()()()()(al)"
+    print(Solution().interpret(command))  # Gooooal
+
+    # Test Case 3
+    command = "(al)G(al)()()G"
+    print(Solution().interpret(command))  # alGalooG
