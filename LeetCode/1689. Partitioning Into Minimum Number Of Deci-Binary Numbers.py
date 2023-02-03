10a3649fff1a070eda8740febdae8fc1a921fbc0
Taehyun Kim <k1m743hyun@icloud.com>
2023-02-03 09:49:14 +0900

    Update LeetCode/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.py

--- LeetCode/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.py
+++ LeetCode/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.py
@@ -1,3 +1,17 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
         return max(list(n))
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    n = "32"
+    print(Solution().minPartitions(n)) # 3
+
+    # Test Case 2
+    n = "82734"
+    print(Solution().minPartitions(n)) # 8
+
+    # Test Case 3
+    n = "27346209830709182346"
+    print(Solution().minPartitions(n)) # 9
