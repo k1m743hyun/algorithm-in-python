8859f645e805ed9671bb4335a12ab1858abae0e0
Taehyun Kim <k1m743hyun@icloud.com>
2023-02-04 20:15:31 +0900

    Update LeetCode/1720. Decode XORed Array.py

--- LeetCode/1720. Decode XORed Array.py
+++ LeetCode/1720. Decode XORed Array.py
@@ -7,3 +7,15 @@ class Solution:
         for idx in range(len(encoded)):
             arr.append(arr[idx] ^ encoded[idx])
         return arr
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    encoded = [1, 2, 3]
+    first = 1
+    print(Solution().decode(encoded, first)) # [1, 0, 2, 1]
+
+    # Test Case 2
+    encoded = [6,2,7,3]
+    first = 4
+    print(Solution().decode(encoded, first)) # [4, 2, 0, 7, 4]
