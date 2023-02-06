e433b7e7055556121fca17856cea381481cfb366
Taehyun Kim <k1m743hyun@icloud.com>
2023-02-06 21:36:09 +0900

    Update LeetCode/1929. Concatenation of Array.py

--- LeetCode/1929. Concatenation of Array.py
+++ LeetCode/1929. Concatenation of Array.py
@@ -4,4 +4,13 @@ from typing import List
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         return nums * 2
-      
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    nums = [1, 2, 1]
+    print(Solution().getConcatenation(nums))  # [1, 2, 1, 1, 2, 1]
+
+    # Test Case 2
+    nums = [1, 3, 2, 1]
+    print(Solution().getConcatenation(nums))  # [1, 3, 2, 1, 1, 3, 2, 1]
