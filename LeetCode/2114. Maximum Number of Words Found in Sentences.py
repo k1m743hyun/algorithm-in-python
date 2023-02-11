17da2b35f22c7e413df7c71baa877e0584ed352f
Taehyun Kim <k1m743hyun@icloud.com>
2023-02-11 13:33:37 +0000

    Update LeetCode/2114. Maximum Number of Words Found in Sentences.py

--- LeetCode/2114. Maximum Number of Words Found in Sentences.py
+++ LeetCode/2114. Maximum Number of Words Found in Sentences.py
@@ -4,3 +4,12 @@ from typing import List
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
         return max([len(sentence.split()) for sentence in sentences])
+
+if __name__ == '__main__':
+  # Test Case 1
+  sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+  print(Solution().mostWordsFound(sentences)) # 6
+
+  # Test Case 2
+  sentences = ["please wait", "continue to fight", "continue to win"]
+  print(Solution().mostWordsFound(sentences)) # 3
