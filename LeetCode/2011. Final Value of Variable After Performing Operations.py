4ef26533fe69c505e54f348d38ba3ddba961efe7
Taehyun Kim <k1m743hyun@icloud.com>
2023-02-12 20:49:35 +0900

    Update LeetCode/2011. Final Value of Variable After Performing Operations.py

--- LeetCode/2011. Final Value of Variable After Performing Operations.py
+++ LeetCode/2011. Final Value of Variable After Performing Operations.py
@@ -16,20 +16,19 @@ class Solution:
                     result += 1
                 else:
                     result -= 1
-            
+
         return result
 
 
 if __name__ == '__main__':
     # Test Case 1
     operations = ["--X", "X++", "X++"]
-    print(Solution().finalValueAfterOperations(operations)) # 1
+    print(Solution().finalValueAfterOperations(operations))  # 1
 
     # Test Case 2
     operations = ["++X", "++X", "X++"]
-    print(Solution().finalValueAfterOperations(operations)) # 3
+    print(Solution().finalValueAfterOperations(operations))  # 3
 
     # Test Case 3
     operations = ["X++", "++X", "--X", "X--"]
-    print(Solution().finalValueAfterOperations(operations)) # 0
-      
+    print(Solution().finalValueAfterOperations(operations))  # 0
