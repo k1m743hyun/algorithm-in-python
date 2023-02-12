f2697e770931753c4ec186664302c67bbdb03caa
Taehyun Kim <k1m743hyun@icloud.com>
2023-02-12 20:52:39 +0900

    Update LeetCode/2235. Add Two Integers.py

--- LeetCode/2235. Add Two Integers.py
+++ LeetCode/2235. Add Two Integers.py
@@ -1,4 +1,15 @@
 class Solution:
     def sum(self, num1: int, num2: int) -> int:
         return num1 + num2
-      
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    num1 = 12
+    num2 = 5
+    print(Solution().sum(num1, num2))  # 17
+
+    # Test Case 2
+    num1 = -10
+    num2 = 4
+    print(Solution().sum(num1, num2))  # -6
