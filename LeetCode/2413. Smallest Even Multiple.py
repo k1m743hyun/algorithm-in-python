8ceabbb039da91748e6ff85c0fdfd0722ebfba9f
Taehyun Kim <k1m743hyun@icloud.com>
2023-02-12 20:59:02 +0900

    Update LeetCode/2413. Smallest Even Multiple.py

--- LeetCode/2413. Smallest Even Multiple.py
+++ LeetCode/2413. Smallest Even Multiple.py
@@ -1,3 +1,13 @@
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
         return n * 1 if n % 2 == 0 else n * 2
+
+
+if __name__ == '__main__':
+    # Test Case 1
+    n = 5
+    print(Solution().smallestEvenMultiple(n))  # 10
+
+    # Test Case 2
+    n = 6
+    print(Solution().smallestEvenMultiple(n))  # 6
